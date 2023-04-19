2bb8d29585a92e0fe67f510e85cf2e22b1da8527
leekt <leekt216@gmail.com>
2023-04-20 00:05:17 +0900

    running mempool on rpc up

--- packages/cli/src/handler.ts
+++ packages/cli/src/handler.ts
@@ -1,7 +1,10 @@
 import { fromZodError } from "zod-validation-error"
 import { IBundlerArgs, IBundlerArgsInput, bundlerArgsSchema } from "@alto/config"
 import { RpcHandler, Server } from "@alto/api"
+import { EmptyValidator } from "@alto/validator"
 import { createPublicClient, http } from "viem"
+import { MemoryMempool } from "@alto/mempool"
+import { Address } from "@alto/types"
 
 const parseArgs = (args: IBundlerArgsInput): IBundlerArgs => {
     // validate every arg, make typesafe so if i add a new arg i have to validate it
@@ -21,7 +24,16 @@ export const bundlerHandler = async (args: IBundlerArgsInput): Promise<void> => 
             transport: http(args.rpcUrl),
         }
     )
-    const rpcEndpoint = new RpcHandler(client)
+    const addressToValidator = new Map()
+    const mempool = new MemoryMempool(client)
+    parsedArgs.entryPoints.forEach((addr : Address) => {
+        addressToValidator.set(addr, new EmptyValidator(
+            client,
+            addr,
+            mempool
+        ))
+    });
+    const rpcEndpoint = new RpcHandler(client, addressToValidator)
 
     const server = new Server(rpcEndpoint, parsedArgs)
     await server.start()
