b414f487f16c290f4b75d54c2b5ac84d6edbf4bc
Kristof Gazso <kristof.gazso@gmail.com>
2023-06-30 08:33:08 +0100

    decrease gas price

--- packages/executor/src/gasPrice.ts
+++ packages/executor/src/gasPrice.ts
@@ -69,7 +69,7 @@ export async function getGasPrice(
 
     let maxPriorityFeePerGas = 2_000_000_000n > gasPrice ? gasPrice : 2_000_000_000n
     if (chainId === ChainId.LineaTestnet) {
-        gasPrice = gasPrice * 2n
+        gasPrice = (gasPrice * 3n) / 2n
         maxPriorityFeePerGas = gasPrice
     }
 
