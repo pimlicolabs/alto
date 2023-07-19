a2b1c3329638d72180a086a5db54c0f45a39e4e1
Kristof Gazso <kristof.gazso@gmail.com>
2023-07-19 18:25:45 +0200

    update gasprice decoding

--- packages/types/src/gasPrice.ts
+++ packages/types/src/gasPrice.ts
@@ -38,11 +38,11 @@ const gasPrice = z
 export const gasStationResult = z.object({
     safeLow: gasPrice,
     standard: gasPrice,
-    fast: gasPrice,
+    fast: gasPrice
     // @ts-ignore
-    estimatedBaseFee: z.union([z.number(), z.string()]).transform((val) => parseGwei(`${val}`)),
-    blockTime: z.number(),
-    blockNumber: z.number()
+    // estimatedBaseFee: z.union([z.number(), z.string()]).transform((val) => parseGwei(`${val}`)),
+    // blockTime: z.number(),
+    // blockNumber: z.number()
 })
 
 export type GasPriceParameters = z.infer<typeof gasPrice>
