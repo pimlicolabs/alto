3587b4525159323593dc50d253b7ad2e06334f28
Kristof Gazso <kristof.gazso@gmail.com>
2023-08-12 01:20:28 +0100

    remove pushgateway

--- packages/utils/src/metrics.ts
+++ packages/utils/src/metrics.ts
@@ -1,4 +1,4 @@
-import { Gauge, Pushgateway, Registry, collectDefaultMetrics } from "prom-client"
+import { Gauge, Registry, collectDefaultMetrics } from "prom-client"
 
 export function createMetrics(registry: Registry, chainId: number, environment: string) {
     collectDefaultMetrics({
@@ -13,18 +13,6 @@ export function createMetrics(registry: Registry, chainId: number, environment: 
         name: "executor_wallets_available_count",
         help: "Number of available executor wallets to bundle"
     })
-
-    const gateway = new Pushgateway("https://pushgateway-production.up.railway.app/")
-    setInterval(() => {
-        gateway
-            .push({ jobName: "test" })
-            .then(({ resp, body }) => {
-                /* ... */
-            })
-            .catch((err) => {
-                /* ... */
-            })
-    }, 1000 * 10)
 
     return {
         walletsAvailable
