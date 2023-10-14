268aaed6c52ee9dd540ca13a813e0935f75dfdfe
Kristof Gazso <kristof.gazso@gmail.com>
2023-10-14 16:57:57 +0100

    mantle support

--- packages/utils/src/gasPrice.ts
+++ packages/utils/src/gasPrice.ts
@@ -78,7 +78,12 @@ export async function getGasPrice(
         }
     }
 
-    if (chainId === chains.arbitrum.id || chainId === chains.scrollSepolia.id || chainId === chains.arbitrumGoerli.id) {
+    if (
+        chainId === chains.arbitrum.id ||
+        chainId === chains.scrollSepolia.id ||
+        chainId === chains.arbitrumGoerli.id ||
+        chainId === chains.mantle.id
+    ) {
         gasPrice = (gasPrice * 5n) / 4n
         return {
             maxFeePerGas: gasPrice,
