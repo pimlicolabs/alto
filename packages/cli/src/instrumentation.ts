ea6e969a26acecbd0dc24fec1191d4062db567eb
Nikita Melnikov <nikmel2803@gmail.com>
2024-01-02 16:57:07 +0100

    Add OpenTelemetry instrumentations for Fastify, HTTP, and IORedis

--- packages/cli/src/instrumentation.ts
+++ packages/cli/src/instrumentation.ts
@@ -1,17 +1,22 @@
-/*instrumentation.ts*/
 import { NodeSDK } from '@opentelemetry/sdk-node';
-import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
 import { propagation } from '@opentelemetry/api';
 import { W3CTraceContextPropagator } from '@opentelemetry/core';
 import { FetchInstrumentation } from 'opentelemetry-instrumentation-fetch-node';
+import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
+import { IORedisInstrumentation } from '@opentelemetry/instrumentation-ioredis';
+import { FastifyInstrumentation } from '@opentelemetry/instrumentation-fastify';
 
 propagation.setGlobalPropagator(new W3CTraceContextPropagator());
 
 const sdk = new NodeSDK({
     traceExporter: new OTLPTraceExporter(),
     instrumentations: [
-        getNodeAutoInstrumentations(),
+        new HttpInstrumentation({
+            requireParentforOutgoingSpans: true,
+        }),
+        new FastifyInstrumentation(),
+        new IORedisInstrumentation(),
         new FetchInstrumentation({}),
     ],
 });
