8b81ff3117d064c4912b3fba04c7daa8d32ab087
Nikita Melnikov <nikmel2803@gmail.com>
2024-01-04 16:56:51 +0100

    update opentelemetry instrumentation

--- packages/cli/src/instrumentation.ts
+++ packages/cli/src/instrumentation.ts
@@ -1,24 +1,43 @@
-import { NodeSDK } from '@opentelemetry/sdk-node';
-import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
-import { propagation } from '@opentelemetry/api';
-import { W3CTraceContextPropagator } from '@opentelemetry/core';
-import { FetchInstrumentation } from 'opentelemetry-instrumentation-fetch-node';
-import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
-import { IORedisInstrumentation } from '@opentelemetry/instrumentation-ioredis';
-import { FastifyInstrumentation } from '@opentelemetry/instrumentation-fastify';
+import { NodeSDK } from "@opentelemetry/sdk-node"
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-proto"
+import { Sampler, SamplingDecision, ParentBasedSampler } from "@opentelemetry/sdk-trace-base"
+import { Context, Attributes, SpanKind } from "@opentelemetry/api"
+import { SemanticAttributes } from "@opentelemetry/semantic-conventions"
+import { FetchInstrumentation } from "opentelemetry-instrumentation-fetch-node"
+import { HttpInstrumentation } from "@opentelemetry/instrumentation-http"
+import { FastifyInstrumentation } from "@opentelemetry/instrumentation-fastify"
+import { PinoInstrumentation } from "@opentelemetry/instrumentation-pino"
 
-propagation.setGlobalPropagator(new W3CTraceContextPropagator());
+class CustomSampler implements Sampler {
+    shouldSample(context: Context, traceId: string, spanName: string, spanKind: SpanKind, attributes: Attributes) {
+        const ignoredRoutes = ["/metrics", "/health"]
+
+        const httpTarget = attributes[SemanticAttributes.HTTP_TARGET]
+
+        if (
+            spanKind === SpanKind.SERVER && httpTarget && ignoredRoutes.includes(httpTarget.toString())
+        ) {
+            return { decision: SamplingDecision.NOT_RECORD }
+        }
+        // fallback
+        return { decision: SamplingDecision.RECORD_AND_SAMPLED }
+    }
+    toString() {
+        return "CustomSampler"
+    }
+}
 
 const sdk = new NodeSDK({
     traceExporter: new OTLPTraceExporter(),
     instrumentations: [
         new HttpInstrumentation({
-            requireParentforOutgoingSpans: true,
+            requireParentforOutgoingSpans: true
         }),
-        new FastifyInstrumentation(),
-        new IORedisInstrumentation(),
         new FetchInstrumentation({}),
+        new FastifyInstrumentation(),
+        new PinoInstrumentation(),
     ],
-});
+    sampler: new ParentBasedSampler({root: new CustomSampler()})
+})
 
-sdk.start();
+sdk.start()
