0ebb68a683094cc68b65bfb22d58d6d696e9d7f9
Garvit Khatri <garvitdelhi@gmail.com>
2024-02-22 23:23:22 +0530

    enable  check only for v2

--- src/entrypoint-0.6/rpc/validation/UnsafeValidator.ts
+++ src/entrypoint-0.6/rpc/validation/UnsafeValidator.ts
@@ -322,19 +322,21 @@ export class UnsafeValidator implements InterfaceValidator {
     }
 
     async validatePreVerificationGas(userOperation: UserOperation) {
-        const preVerificationGas = await calcPreVerificationGas(
-            this.publicClient,
-            userOperation,
-            this.entryPoint,
-            this.chainId,
-            this.logger
-        )
-
-        if (preVerificationGas > userOperation.preVerificationGas) {
-            throw new RpcError(
-                `preVerificationGas is not enough, required: ${preVerificationGas}, got: ${userOperation.preVerificationGas}`,
-                ValidationErrors.SimulateValidation
+        if (this.apiVersion !== "v1") {
+            const preVerificationGas = await calcPreVerificationGas(
+                this.publicClient,
+                userOperation,
+                this.entryPoint,
+                this.chainId,
+                this.logger
             )
+
+            if (preVerificationGas > userOperation.preVerificationGas) {
+                throw new RpcError(
+                    `preVerificationGas is not enough, required: ${preVerificationGas}, got: ${userOperation.preVerificationGas}`,
+                    ValidationErrors.SimulateValidation
+                )
+            }
         }
     }
 
