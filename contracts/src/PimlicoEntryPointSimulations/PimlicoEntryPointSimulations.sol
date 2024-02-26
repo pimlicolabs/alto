f441e4d365d2cda225ed1c9fdf15a7a017ff4528
Garvit Khatri <garvitdelhi@gmail.com>
2024-02-26 15:51:56 -0700

    add PimlicoEntryPointSimulations

--- contracts/src/PimlicoEntryPointSimulations/PimlicoEntryPointSimulations.sol
+++ contracts/src/PimlicoEntryPointSimulations/PimlicoEntryPointSimulations.sol
@@ -7,18 +7,26 @@ contract PimlicoEntryPointSimulations {
 
     constructor () {}
 
-    function simulateEntryPoint(address payable ep, bytes memory data) public returns(bytes memory returnData) {
-        try EntryPoint(ep).delegateAndRevert(address(eps), data)
-        {} catch {
-            assembly ("memory-safe") {
-                let len := returndatasize()
-                let ptr := mload(0x40)
-                mstore(0x40, add(ptr, add(len, 0x20)))
-                mstore(ptr, len)
-                returndatacopy(add(ptr, 0x20), 0, len)
-                returnData := ptr
+    function simulateEntryPoint(address payable ep, bytes[] memory data) public returns(bytes[] memory) {
+        bytes[] memory returnDataArray = new bytes[](data.length + 1);
+
+        for(uint i = 0; i < data.length; i++) {
+            bytes memory returnData;
+            try EntryPoint(ep).delegateAndRevert(address(eps), data[i])
+            {} catch {
+                assembly ("memory-safe") {
+                    let len := returndatasize()
+                    let ptr := mload(0x40)
+                    mstore(0x40, add(ptr, add(len, 0x20)))
+                    mstore(ptr, len)
+                    returndatacopy(add(ptr, 0x20), 0, len)
+                    returnData := ptr
+                }
             }
+            returnDataArray[i] = returnData;
         }
+
+        return returnDataArray;
     }
 
 }
