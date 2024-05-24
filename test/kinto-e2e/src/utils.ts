40ad28b011d687ab7777aa93a94ebb9fdd740482
mouseless <97399882+mouseless-eth@users.noreply.github.com>
2024-05-24 19:02:05 +0100

    setup kinto test rpc

--- test/kinto-e2e/src/utils.ts
+++ test/kinto-e2e/src/utils.ts
@@ -21,8 +21,7 @@ export const kintoMainnet = defineChain({
 })
 
 export const KINTO_ENTRYPOINT = "0x2843C269D2a64eCfA63548E8B3Fc0FD23B7F70cb"
-export const KINTO_RPC =
-    "https://7887.rpc.thirdweb.com/9fc39d5e2a3e149cc31cf9625806a2fe"
+export const KINTO_RPC = process.env.KINTO_RPC
 
 export const sleep = async (ms: number) => {
     await new Promise((resolve) => setTimeout(resolve, ms))
