0335e0bbc99d72476a9b9f4eb2404aed831b480a
mouseless <97399882+mouseless-eth@users.noreply.github.com>
2024-06-12 00:14:11 +0100

    ensure gas never results in a aa95 revert

--- src/executor/executor.ts
+++ src/executor/executor.ts
@@ -584,8 +584,6 @@ export class Executor {
                 childLogger
             )
 
-        gasLimit += 10_000n
-
         if (resubmitAllOps) {
             this.markWalletProcessed(wallet)
             return opsWithHashes.map((owh) => {
@@ -646,6 +644,20 @@ export class Executor {
             ),
             entryPoint
         })
+
+        // sometimes the estimation rounds down, adding a fixed constant accounts for this
+        gasLimit += 10_000n
+
+        if (isUserOpVersion06) {
+            // https://github.com/eth-infinitism/account-abstraction/blob/fa61290d37d079e928d92d53a122efcc63822214/contracts/core/EntryPoint.sol#L236
+            let innerHandleOpFloor = 0n
+            for (const owh of opsWithHashToBundle) {
+                const op = deriveUserOperation(owh.mempoolUserOperation)
+                innerHandleOpFloor +=
+                    op.callGasLimit + op.verificationGasLimit + 5000n
+            }
+            maxBigInt(gasLimit, innerHandleOpFloor)
+        }
 
         childLogger.trace({ gasLimit }, "got gas limit")
 
