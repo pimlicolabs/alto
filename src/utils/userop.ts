17a717c8acb8b0cf98d3d77b955bf4e5f0c71713
mouseless <97399882+mouseless-eth@users.noreply.github.com>
2024-07-18 14:36:45 +0100

    fix comment

--- src/utils/userop.ts
+++ src/utils/userop.ts
@@ -194,7 +194,7 @@ type UserOperationDetailsType = {
 
 type BundlingStatus =
     | {
-          // The tx was successfully mined (all ops passed EntryPoint validation)
+          // The tx was successfully mined
           // The status of each userOperation is recorded in userOperaitonDetails
           status: "mined"
           userOperationDetails: Record<Hex, UserOperationDetailsType>
