b469467c5e0b580c1685a82c32059c06db1555f1
mouseless <97399882+mouseless-eth@users.noreply.github.com>
2024-09-09 11:27:47 +0100

    cleanup contracts

--- contracts/src/TestUtils/AlwaysRevert.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.8.20;
-
-// @dev Contract to test how bundler handles onchain reverts
-contract AlwaysRevert {
-    function revertWithMessage(string memory message) public pure {
-        revert(message);
-    }
-}
