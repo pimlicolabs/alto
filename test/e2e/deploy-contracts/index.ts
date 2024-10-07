2e394182b6bb2f3aa9274d0dca09c9811bda7ad8
mouseless <97399882+mouseless-eth@users.noreply.github.com>
2024-10-07 11:48:59 +0100

    update simulations contract

--- test/e2e/deploy-contracts/index.ts
+++ test/e2e/deploy-contracts/index.ts
@@ -223,7 +223,7 @@ const main = async () => {
         "0x4e59b44847b379578588920ca78fbf26c0b4956c",
         "0x0000000071727De22E5E9d8BAf0edAc6f37da032",
         "0x91E60e0613810449d098b0b5Ec8b51A0FE8c8985",
-        "0xf0763f1faa2cc2539ca3e5f55795cb58987c8021", // 0.7 Simulations Contract
+        "0xBbe8A301FbDb2a4CD58c4A37c262ecef8f889c47", // 0.7 Simulations Contract
         BUNDLE_BULKER_ADDRESS,
         PER_OP_INFLATOR_ADDRESS,
         SIMPLE_INFLATOR_ADDRESS
