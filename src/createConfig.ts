3de205122d20f99d56af5a0e7154e2b3a3317a06
Garvit Khatri <garvitdelhi@gmail.com>
2024-10-14 03:06:47 +0100

    Make config readonly

--- src/createConfig.ts
+++ src/createConfig.ts
@@ -3,10 +3,10 @@ import type { CamelCasedProperties } from "./cli/parseArgs"
 import type { Logger } from "pino"
 import type { Chain, PublicClient, Transport, WalletClient } from "viem"
 
-export type AltoConfig = CamelCasedProperties<IOptions> & {
-    logger: Logger
-    publicClient: PublicClient<Transport, Chain>
-    walletClient: WalletClient<Transport, Chain>
+export type AltoConfig = Readonly<CamelCasedProperties<IOptions>> & {
+    readonly logger: Logger
+    readonly publicClient: PublicClient<Transport, Chain>
+    readonly walletClient: WalletClient<Transport, Chain>
 }
 
 export function createConfig(
