1548d14af0bebd966dd995efffeff78d4cda27b4
Garvit Khatri <garvitdelhi@gmail.com>
2024-12-20 13:47:42 +0000

    update contracts

--- test/e2e/deploy-contracts/index.ts
+++ test/e2e/deploy-contracts/index.ts
@@ -225,7 +225,7 @@ const main = async () => {
         "0x4e59b44847b379578588920ca78fbf26c0b4956c",
         "0x0000000071727De22E5E9d8BAf0edAc6f37da032",
         "0x91E60e0613810449d098b0b5Ec8b51A0FE8c8985",
-        "0xfF802E602b328525883389801231cCeBbAe4d2f6", // 0.7 Simulations Contract
+        "0xe1b9bcD4DbfAE61585691bdB9A100fbaAF6C8dB0", // 0.7 Simulations Contract
         BUNDLE_BULKER_ADDRESS,
         PER_OP_INFLATOR_ADDRESS,
         SIMPLE_INFLATOR_ADDRESS
