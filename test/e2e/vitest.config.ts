10c8d8590e7f39175ed6ec7e4172df14fd8ca884
mouseless <97399882+mouseless-eth@users.noreply.github.com>
2025-05-11 22:44:03 +0100

    restore vitest

--- test/e2e/vitest.config.ts
+++ test/e2e/vitest.config.ts
@@ -25,10 +25,7 @@ export default defineConfig({
         globalSetup: join(__dirname, "./setup.ts"),
         environment: "node",
         testTimeout: 60_000,
-        hookTimeout: 45_000,
-        include: ["**/eth_estimateUserOperationGas.test.ts"],
-        // Only run the revert reason test
-        testNamePattern: "Should throw revert reason if simulation reverted during callphase"
+        hookTimeout: 45_000
         // setupFiles: [join(__dirname, "./setup.ts")],
         // globalSetup: [join(__dirname, "./globalSetup.ts")]
     }
