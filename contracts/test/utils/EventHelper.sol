55c6451e1edce212e64d4cf7b9a806879417d3af
mouseless <97399882+mouseless-eth@users.noreply.github.com>
2025-06-19 22:15:40 +0100

    refactor(EventHelper): remove unused event and functions

--- contracts/test/utils/EventHelper.sol
+++ contracts/test/utils/EventHelper.sol
@@ -3,24 +3,13 @@ pragma solidity ^0.8.0;
 
 contract EventHelper {
     event MessageEmitted(string message);
-    event MessageWithIndexEmitted(string indexed message);
     event MessageWithSenderEmitted(string message, uint256 value, address sender);
 
     function emitMessage(string memory message) external {
         emit MessageEmitted(message);
     }
 
-    function emitIndexedMessage(string memory message) external {
-        emit MessageWithIndexEmitted(message);
-    }
-
     function emitMultipleData(string memory message, uint256 value) external {
         emit MessageWithSenderEmitted(message, value, msg.sender);
-    }
-
-    function emitMultipleMessages(string[] memory messages) external {
-        for (uint256 i = 0; i < messages.length; i++) {
-            emit MessageEmitted(messages[i]);
-        }
     }
 }
